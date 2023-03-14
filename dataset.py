d3da17830edf5d53424218780b20d46a18f6e43f
Zihan Ling <lzh1020@hotmail.com>
2023-03-14 16:11:06 -0400

    init setup

---
+++ dataset.py
@@ -0,0 +1,7 @@
+
+class NerfDataset():
+    def __init__(self):
+        return None 
+    
+    def __getitem__(self):
+        return None 
---
+++ network.py
@@ -0,0 +1,9 @@
+import torch.nn as nn 
+
+class NerfModel(nn.Module):
+    def __init__(self):
+        super(NerfModel, self).__init__()
+
+    def forward(self, rays):
+        return None
+    
