3f3610bb2bb02f8d66fc2a5dc34912b3fe36f93b
Zihan Ling <lzh1020@hotmail.com>
2023-03-15 10:45:20 -0400

    selena chnages

--- nerf/nerf.py
+++ nerf/nerf.py
@@ -1,1 +1,17 @@
-"""the MLP for NeRF""" 
+
+import torch.nn as nn 
+
+class NerfModel(nn.Module):
+    """the MLP for NeRF""" 
+    def __init__(self):
+        super(NerfModel, self).__init__()
+
+        # Kinjal 
+        # self.mlp = self.build_network() 
+
+    def build_network(self):
+        return None 
+    
+    def forward(self, x):
+        
+
